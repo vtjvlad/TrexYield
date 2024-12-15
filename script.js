da87a24b772246b77c066e8cb2bf5260fd95f9eb
vtjvlad <vtj.vlad@gmail.com>
2024-12-15 22:19:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiUrl = 'https://165.154.244.219'; // URL вашего сервера
+const apiUrl = 'https://vtjvlad.ddns.net'; // URL вашего сервера
 
 function showRegistrationForm() {
     document.getElementById('registration-form').style.display = 'block';
